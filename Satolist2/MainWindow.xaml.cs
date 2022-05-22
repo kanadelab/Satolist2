19d53da503179580a685d646cb096db9c3accc53
ななっち <nanachi@hotmail.co.jp>
2022-05-22 20:51:33 +0900

    デプロイビルドのみ更新チェックするように

--- Satolist2/MainWindow.xaml.cs
+++ Satolist2/MainWindow.xaml.cs
@@ -1515,7 +1515,7 @@ namespace Satolist2
 
 		private void CheckNetworkUpdate(bool acceptReleaseBuild)
 		{
-#if DEPLOY || true
+#if DEPLOY
 			Task.Run(() =>
 			{
 				var release = UpdateChecker.GetRelease(acceptReleaseBuild);
