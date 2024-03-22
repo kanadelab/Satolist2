1cc2235917e7b805a4efe502891ac2d6c87c8449
ななっち <nanachi@hotmail.co.jp>
2024-03-22 23:29:56 +0900

    さとりすと 2.1.4

--- Satolist2/Properties/AssemblyInfo.cs
+++ Satolist2/Properties/AssemblyInfo.cs
@@ -51,5 +51,5 @@ using System.Windows;
 // すべての値を指定するか、次を使用してビルド番号とリビジョン番号を既定に設定できます
 // 既定値にすることができます:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.1.3.1")]
-[assembly: AssemblyFileVersion("2.1.3.1")]
+[assembly: AssemblyVersion("2.1.4.0")]
+[assembly: AssemblyFileVersion("2.1.4.0")]
